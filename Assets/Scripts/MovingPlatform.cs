c02d2897788ab5bbadc3559a7779ff3bbb95f60b
Pedro Sousa Meireles <psmeireles25@gmail.com>
2020-01-20 19:21:34 -0300

    Implemented a moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+    [SerializeField]
+    private float _speed = 5f;
+
+    private Vector3 _currentTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (transform.position == _targetA.position)
+            _currentTarget = _targetB.position;
+        else if (transform.position == _targetB.position)
+            _currentTarget = _targetA.position;
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ("Player".Equals(other.tag))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ("Player".Equals(other.tag))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
